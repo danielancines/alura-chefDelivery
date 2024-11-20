4770d494ea4eea55d409dacfc04bc2337fdc9672
Daniel Ancines <daniel.ancines@gmail.com>
2024-11-20 11:36:21 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Daniel Ancines on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChefDeliveryTests/ChefDeliveryTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryTests.swift
+//  ChefDeliveryTests
+//
+//  Created by Daniel Ancines on 20/11/24.
+//
+
+import Testing
+@testable import ChefDelivery
+
+struct ChefDeliveryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
