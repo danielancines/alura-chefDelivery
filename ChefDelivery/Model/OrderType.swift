4b0a234f8289d2ffe24eb4a1c664b5d38148591a
Daniel Ancines <daniel.ancines@gmail.com>
2025-03-22 21:36:59 -0300

    GridView, Layouts e Components

---
+++ ChefDelivery/Model/OrderType.swift
@@ -0,0 +1,14 @@
+//
+//  OrderType.swift
+//  ChefDelivery
+//
+//  Created by Daniel Ancines on 22/03/25.
+//
+
+import Foundation
+
+struct OrderType: Identifiable {
+    let id: Int
+    let name: String
+    let image: String
+}
